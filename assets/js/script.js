59d3fce8065217b6e9334e606e78a4f7afce7e48
eji_a <156595423+e-aji@users.noreply.github.com>
2024-02-13 21:53:21 +0000

    more jave updates and some file deletion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-var startQuizEl = document.getElementById("startQuiz")
+var startQuizEl = document.getElementById("startQuiz");
 
-var welcomeEl = document.getElementById("welcome")
-var quizEl = document.getElementById("quiz")
-var resultEl = document.getElementById ("result")
-var optionsEl = document.getElementById("options")
-var messageEl = document.getElementById("message")
-var timerEl = document.getElementById("timer")
-var summaryEl = document.getElementById("summary")
+var welcomeEl = document.getElementById("welcome");
+var quizEl = document.getElementById("quiz");
+var resultEl = document.getElementById("result");
+var optionsEl = document.getElementById("options");
+var messageEl = document.getElementById("message");
+var timerEl = document.getElementById("timer");
+var summaryEl = document.getElementById("summary");
 var initialsEl = document.getElementById("initials");
 
 var secondsLeft = 0;
@@ -14,127 +14,119 @@ var score = 0;
 var currentQuestion = 0;
 var countdownTimer;
 
+//Define quiz questions, choices and answers 
 var questions = [
-
-    {
-        title: 'Commonly used data types DO NOT include:',
-        choices: ['strings', 'booleans', 'alerts', 'numbers'],
-        answer: 'alerts',
-    },
-
-    {
-        title: 'The condition in an if/else statement is enclosed within ____.',
-        choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
-        answer: 'alerts',
-
-    }, 
-
-   {    title: 'Arrays in JavaScript an be used to store ____', 
-        choices: [
-            'numbers and strings',
-            'other arrays', 
-            'booleans',
-            'all of the above',
-        ], 
-        answer: 'all of the above', 
-    }, 
-
-    {   title: 'String values must be enclosed within ____ when being assigned to variable',
-        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
-        answer: 'quotes',
-    }, 
-
-    {
-        title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-        answer: "console.log"
-    },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    title: "The condition in an if/else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above",
+  },
+  {
+    title: "String values must be enclosed within ____ when being assigned to a variable",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
+  },
+  {
+    title:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+    answer: "console.log",
+  },
 ];
 
-function displayMessage (msg){
-    messageEl.textContent = msg;
+function displayMessage(msg) {
+  messageEl.textContent = msg;
 }
+
 // Event listener for when the user clicks "Start Quiz" button
-startQuizEl.addEventListener ("click", onStartGame);
+startQuizEl.addEventListener("click", onStartGame);
 
-// Function to start game 
-function onStartGame(){
+// Function to start game
+function onStartGame() {
+  startQuizEl.style.display = "none";
+  welcomeEl.style.display = "none";
+  quizEl.style.display = "block";
 
-    startQuizEl.style.display = "none";
-    welcomeEl.style.display = "none";
-    quizEl.style.display = "block";
-
-    displayQuestion ();
-    setTimer ();
+  displayQuestion();
+  setTimer();
 }
+
 // Function to set timer
-function setTimer(){
-    secondsLeft = 60;
+function setTimer() {
+  secondsLeft = 60;
 
-    countdownTimer = setInterval(function () {
-        secondsLeft--;
-        // Countdown and display how many seconds left 
-        if (secondsLeft > 0) {
-            timerEl.textContent = secondsLeft = " seconds remaining";
-        }else if (secondsLeft === 1) {
-            timerEl.textContent = secondsLeft + " second remaining";
-        }else {
-            // Stop the counter and end the game when time runs out
-            stopGame();
-        }
-   }, 1000);
-
-}
-//Function to display questions 
-
-function displayQuestion () {
-
-    var questions = questions[currentQuestion];
-    var options = question.choices;
-
-    console.log ('current question is ') + score;
-
-    if (currentQuestion >= questions.length) {
-
-        stopGame();
-
-        return;
+  countdownTimer = setInterval(function () {
+    secondsLeft--;
+    // Countdown and display how many seconds left
+    if (secondsLeft > 1) {
+      timerEl.textContent = secondsLeft + " seconds remaining";
+    } else if (secondsLeft === 1) {
+      timerEl.textContent = secondsLeft + " second remaining";
+    } else {
+      // Stop the counter and end the game when time runs out
+      stopGame();
     }
+  }, 1000);
+}
+
+//Function to display questions
+function displayQuestion() {
+  var question = questions[currentQuestion];
+  var options = question.choices;
+
+  optionsEl.innerHTML = ""; // Clear previous options
+
+  document.getElementById("question").textContent = question.title;
+
+  options.forEach(function (option) {
+    var button = document.createElement("button");
+    button.textContent = option;
+    button.addEventListener("click", onSelectAnswer);
+    optionsEl.appendChild(button);
+  });
 }
 
 function onSelectAnswer(e) {
+  var correctAnswer = questions[currentQuestion].answer;
+  var userAnswer = e.target.textContent;
 
-    var correctanswer = questions[currentQuestion].answer;
-    var userAnswer = e.target.textContent;
-
-    if (correctanswer === userAnswer) {
-        score++;
-        displayMessage("Correct!");
-    }   else {
-        displayMessage("Wrong!");
-        secondsLeft -= 10;
-        if (secondsLeft < 0) {
-            secondsLeft = 0
-        }
+  if (correctAnswer === userAnswer) {
+    score++;
+    displayMessage("Correct!");
+  } else {
+    displayMessage("Wrong!");
+    secondsLeft -= 10;
+    if (secondsLeft < 0) {
+      secondsLeft = 0;
     }
+  }
 
-    currentQuestion++; 
-    if (currentQuestion < questions.length){
-        displayQuestion();
-    } else {
-        stopGame();
-    }
+  currentQuestion++;
+  if (currentQuestion < questions.length) {
+    displayQuestion();
+  } else {
+    stopGame();
+  }
 }
+
 // Function to stop game
 function stopGame() {
+  clearInterval(countdownTimer);
 
-    clearInterval (countdownTimer);
+  timerEl.textContent = "";
 
-    timerEl.textContent = "";
+  quizEl.style.display = "none";
+  resultEl.style.display = "block";
 
-    quizEl.style.display = "none";
-    resultEl.style.display - "block";
-
-    summaryEl.textContent = "Your Score is: " + score;
+  summaryEl.textContent = "Your Score is: " + score;
 }
